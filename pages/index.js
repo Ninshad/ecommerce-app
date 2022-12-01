1e053cdbf533e1f5ad2eaef155a6ff2ffee6b2d8
Ninshad <ninshad7@gmail.com>
2022-12-01 18:39:20 +0530

    1/12/22

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { Product, FooterBanner, HeroBanner } from '../components';
 export default function Home() {
   return (
     <div >
-      <HeroBanner />
+      {/* <HeroBanner /> */}
      <div className='products-heading'>
       <h1>Best selling products</h1>
      </div>
