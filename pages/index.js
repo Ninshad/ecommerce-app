ce203a041ade37e51ebb3a9442942cfd080bf5cc
Ninshad <ninshad7@gmail.com>
2022-12-02 18:07:12 +0530

    2/12/22

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import { Product, FooterBanner, HeroBanner } from '../components';
+import { client } from '../lib/client';
 
-export default function Home() {
+const Home = ({ products, bannerData }) => {
+
   return (
     <div >
-      {/* <HeroBanner /> */}
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      {console.log('BannerData', bannerData)}
      <div className='products-heading'>
       <h1>Best selling products</h1>
      </div>
@@ -12,3 +15,17 @@ export default function Home() {
     </div>
   )
 }
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
+}
+
+export default Home;
